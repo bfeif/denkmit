d9fb88a95efff357c80ca27cb68570c613a81906
Ben Feifke <benfeifke@gmail.com>
2021-05-02 00:36:07 +0200

    Added stuff.

--- denkmit/settings.py
+++ denkmit/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'denkmit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flashcards',
+        'USER': 'denkmit',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ flashcards/models.py
@@ -0,0 +1,17 @@
+# from django.db import models
+
+# class Noun(models.Model):
+#     noun_de = models.CharField(max_length=20)
+#     noun_en = models.CharField(max_length=20)
+#     noun_de_pl = models.CharField(max_length=20)
+#     pluralization_model = models.CharField(max_length=20)
+#     gender = models.CharField(max_length=1) # M, F, N
+
+# class Article(models.Model):
+#     article_de = models.CharField(max_length=3) # the article in german
+#     article_en = models.CharField(max_length=10) # the article in german english
+#     definite = models.BooleanField() # definite or indefinite
+#     case = models.CharField(max_length=3) # Nom, Akk, Dat, Gen
+#     gender = models.CharField(max_length=1) # M, F, N
+
+# class 
