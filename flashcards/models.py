1858e47e0d4593d4ae403366a6eb754a4cefabba
Ben Feifke <benfeifke@gmail.com>
2021-05-02 03:09:22 +0200

    Added unique_together constraints and __str__ methods to each model.

--- flashcards/models.py
+++ flashcards/models.py
@@ -8,12 +8,24 @@ class Noun(models.Model):
     pluralization_model = models.CharField(max_length=20) # TODO change to Integer choice
     gender = models.CharField(max_length=1) # # TODO change to Integer choice: M, F, N
 
+    def __str__(self):
+        gender_dic = {"M": "der", "F": "die", "N": "das", "P": "die"}
+        return f"{gender_dic[self.gender]} {self.noun_de} (die {self.noun_de_pl}) "
+
+
 class PersonalPronoun(models.Model):
     pronoun_de = models.CharField(max_length=20)
     pronoun_en = models.CharField(max_length=20)
     case = models.CharField(max_length=3) # TODO change to Integer choice: Nom, Akk, Dat, Gen
     nom_pronoun_de = models.CharField(max_length=10, default="temp") # TODO change to Integer choice (ich, du, er, sie, es, wir, ihr, Sie)
     order = models.CharField(max_length=10, default="temp") # TODO change to Integer choice (ich, du, er_sie_es, wir, ihr, Sie)
+
+    def __str__(self):
+        return f"({self.nom_pronoun_de}, {self.case}): {self.pronoun_de}"
+    
+    class Meta:
+        unique_together = ("nom_pronoun_de", "case")
+
 
 class Article(models.Model):
     article_de = models.CharField(max_length=10) # the article in german
@@ -28,11 +40,19 @@ class Article(models.Model):
     class Meta:
         unique_together = ("case", "gender", "definite")
 
+
 class Verb(models.Model):
     verb_de = models.CharField(max_length=20)
     verb_en = models.CharField(max_length=20)
-    tense = models.CharField(max_length=10) # TODO change to Integer choice
+    infinitive_de = models.CharField(max_length=20, default="temp")
     mood = models.CharField(max_length=10) # TODO change to Integer choice
+    tense = models.CharField(max_length=10) # TODO change to Integer choice
     subject = models.CharField(max_length=10) # TODO change to Integer choice (ich, du, er_sie_es, wir, ihr, Sie)
     valency = models.IntegerField() # TODO change to Integer choice as per https://en.wikipedia.org/wiki/Verb#Valency
     # TODO: phrasal verbs and the requisite fields
+
+    def __str__(self):
+        return f"({self.infinitive_de}, {self.mood}, {self.tense}, {self.subject}): {self.verb_de}"
+
+    class Meta:
+        unique_together = ("infinitive_de", "mood", "tense", "subject") # every verb conjugation has an infinitive, a grammatical mood, a temporal-tense, and a subject that's conjugating it.
