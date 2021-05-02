fe89b282ac4c2e1304379b157745dbc99eff9a9b
Ben Feifke <benfeifke@gmail.com>
2021-05-02 02:02:14 +0200

    Added stuff to models.

--- flashcards/models.py
+++ flashcards/models.py
@@ -4,6 +4,7 @@ class Noun(models.Model):
     noun_de = models.CharField(max_length=20)
     noun_en = models.CharField(max_length=20)
     noun_de_pl = models.CharField(max_length=20)
+    noun_en_pl = models.CharField(max_length=20, default="temp")
     pluralization_model = models.CharField(max_length=20) # TODO change to Integer choice
     gender = models.CharField(max_length=1) # # TODO change to Integer choice: M, F, N
 
@@ -11,7 +12,8 @@ class PersonalPronoun(models.Model):
     pronoun_de = models.CharField(max_length=20)
     pronoun_en = models.CharField(max_length=20)
     case = models.CharField(max_length=3) # TODO change to Integer choice: Nom, Akk, Dat, Gen
-    gender = models.CharField(max_length=1) # TODO change to Integer choice: M, F, N
+    nom_pronoun_de = models.CharField(max_length=10, default="temp") # TODO change to Integer choice (ich, du, er, sie, es, wir, ihr, Sie)
+    order = models.CharField(max_length=10, default="temp") # TODO change to Integer choice (ich, du, er_sie_es, wir, ihr, Sie)
 
 class Article(models.Model):
     article_de = models.CharField(max_length=3) # the article in german
@@ -25,6 +27,6 @@ class Verb(models.Model):
     verb_en = models.CharField(max_length=20)
     tense = models.CharField(max_length=10) # TODO change to Integer choice
     mood = models.CharField(max_length=10) # TODO change to Integer choice
-    subject = models.CharField(max_length=10) # TODO change to Integer choice
+    subject = models.CharField(max_length=10) # TODO change to Integer choice (ich, du, er_sie_es, wir, ihr, Sie)
     valency = models.IntegerField() # TODO change to Integer choice as per https://en.wikipedia.org/wiki/Verb#Valency
     # TODO: phrasal verbs and the requisite fields
