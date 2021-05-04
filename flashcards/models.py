a7287fc6ac9eaebd4c78f7b3b55649c82f5634bd
Ben Feifke <benfeifke@gmail.com>
2021-05-04 23:34:51 +0200

    Added CLI for reviewing flashcard deck; updated poetry.lock; updated PersonalPronoun unique constraint.

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import time
 
 
 # POS models
@@ -20,12 +21,14 @@ class PersonalPronoun(models.Model):
     pronoun_en = models.CharField(max_length=20)
     case = models.CharField(max_length=3) # TODO change to Integer choice: Nom, Akk, Dat, Gen
     nom_pronoun_de = models.CharField(max_length=10, default="temporary") # TODO change to Integer choice (ich, du, er, sie, es, wir, ihr, Sie)
+    person = models.IntegerField() # 1st person, 2nd person, 3rd person
+    is_plural = models.BooleanField(default=False)
 
     def __str__(self):
         return f"({self.nom_pronoun_de}, {self.case}): {self.pronoun_de}"
     
     class Meta:
-        unique_together = ("nom_pronoun_de", "case")
+        unique_together = ("nom_pronoun_de", "case", "person", "is_plural")
 
 
 class Article(models.Model):
@@ -68,6 +71,42 @@ class RevLog(models.Model):
     def __str__(self):
         return f"""\n- study_timestamp: {self.timestamp}\n- study_duration: {self.duration}\n- study_rating: {self.rating}"""
 
+    @classmethod
+    def rate_flashcard_string(self):
+        return (
+            "Rate difficulty (1-4)\n" \
+            "  - 1 (No idea; show it again this session)\n" \
+            "  - 2 (Difficult)\n" \
+            "  - 3 (Good)\n" \
+            "  - 4 (Easy)\n" \
+            "Difficulty: ")
+
+    # function to generate and study a flashcard
+    @classmethod
+    def run_flashcard(cls, noun):
+        
+        # generate the flashcard
+        rev_log = cls(duration=5, rating=2, noun=noun)
+        
+        # show the question
+        print(rev_log.flashcard_question_str())
+        flashcard_start_timestamp = time.time()
+
+        # wait for the user to request showing the answer, then show it
+        input("Press any key to show the answer...")
+        print(rev_log.flashcard_question_str())
+        print(rev_log.flashcard_answer_str())
+
+        # get the user's rating
+        print(cls.rate_flashcard_string())
+        rating = input()
+        duration = time.time() - flashcard_start_timestamp
+
+        # save the rev_log
+        rev_log.duration = duration
+        rev_log.rating = rating
+        rev_log.save()
+
 
 class PersonalPronoun_Verb_Article_Noun_RevLog(RevLog):
     personal_pronoun = models.ForeignKey('PersonalPronoun', on_delete=models.SET_NULL, null=True) # TODO change to make on_delete plug in the foreign key's word (i.e. instead of id)
@@ -82,32 +121,24 @@ class NounGenderGuess_RevLog(RevLog):
     def __str__(self):
         return f"""{self.noun}:{super().__str__()}"""
 
-    @staticmethod
     def flashcard_question_str(self):
         return f"___ {self.noun.noun_de}"
 
-    @staticmethod
     def flashcard_answer_str(self):
         article = Article.objects.get(gender=self.noun.gender, case="Nom", definite=True).article_de
         return f"{article} {self.noun.noun_de} (die {self.noun.noun_de_pl})"
 
-    # function to generate a new flashcard for studying
-    @classmethod
-    def run_flashcard(cls, noun):
-        rev_log = cls(duration=5, rating=2, noun=noun)
-        print(rev_log)
-        rev_log.save()
-
+    # function to generate a flashcard deck for the day of studying
     @classmethod
     def run_flashcard_deck(cls):
 
         # get the deck of nouns for the study session
-        nouns = Noun.objects.all()
+        nouns = Noun.objects.all() # TODO: change to be only the relevant flashcards, i.e. the ones that need to be practiced for the day.
         deck_length=nouns.count()
 
         # do a flashcard for each noun in the deck
         for index, noun in enumerate(nouns):
             print(f"running flashcard {index}!")
             cls.run_flashcard(noun)
-            input()
             print("------------\n")
+    
