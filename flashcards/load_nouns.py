386ce6c24a516ffdae016261a70f733c161164a0
Ben Feifke <benfeifke@gmail.com>
2021-05-15 11:21:40 +0200

    Updated noun-load-script.

--- flashcards/load_nouns.py
+++ flashcards/load_nouns.py
@@ -1,4 +1,7 @@
-from flashcards.models import Noun
+from flashcards.models import Noun, NounGenderGuess_Score, \
+    NounPluralizationGuess_Score
+
+card_types = [NounGenderGuess_Score, NounPluralizationGuess_Score]
 
 word_de = ["Haus", "Strand", "Lösung"]
 word_en = ["house", "beach", "solution"]
@@ -7,11 +10,20 @@ word_en_pl = ["houses", "beaches", "solutions"]
 gender = ["N", "M", "F"]
 
 for i in range(len(word_de)):
-	n = Noun(
-		word_de = word_de[i],
-		word_en = word_en[i],
-		word_de_pl = word_de_pl[i],
+
+    # create noun
+    n = Noun(
+        word_de = word_de[i],
+        word_en = word_en[i],
+        word_de_pl = word_de_pl[i],
         word_en_pl = word_en_pl[i],
-		gender = gender[i]
-	)
-	n.save()
+        gender = gender[i]
+    )
+
+    # save noun
+    n.save()
+
+    # create and save cards
+    for card_type in card_types:
+        card = card_type(pos=n)
+        card.save()
